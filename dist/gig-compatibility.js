d03dc917ec00d4104f3da1732ce1f0fa6128f05e
Pedro Peña <pedro.jose@gigigo.com>
2015-06-18 16:49:04 +0200

    Update Version

--- dist/gig-compatibility.js
+++ dist/gig-compatibility.js
@@ -291,16 +291,15 @@
     return bowser
 });
 
-Modernizr.addTest('no-support', function() {
+Modernizr.addTest('support', function() {
 
-    var noSupport = false;
-    var v, ver = false;
+    var support = true;
 
     if (supportSystem) {
         if (supportSystem.browser) {
             for (var i in supportSystem.browser) {
                 if (bowser[i] && bowser.version <= supportSystem.browser[i]) {
-                    noSupport = true;
+                    support = false;
                 }
             }
         }
@@ -314,7 +313,7 @@ Modernizr.addTest('no-support', function() {
 
 
                     if (ver && ver <= supportSystem.system.ios) {
-                        noSupport = true;
+                        support = false;
                     }
                 }
 
@@ -325,10 +324,10 @@ Modernizr.addTest('no-support', function() {
                             var androidversion = parseFloat(ua.slice(ua.indexOf("Android") + 8));
 
                             if (supportSystem.system.android <= androidversion) {
-                                noSupport = true;
+                                support = false;
                             } else {
                                 if (!supportSystem.browser.android && !bowser.chrome) {
-                                    noSupport = true;
+                                    support = false;
                                 }
                             }
                         }
@@ -340,14 +339,13 @@ Modernizr.addTest('no-support', function() {
 
         }
 
-        return noSupport;
+        return support;
 
     } else {
-        return noSupport;
+        return support;
     }
 
 });
-
 
 
 
